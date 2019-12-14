4104bb63277832c8d2a20202284d66c48d719319
rainoverme002 <bagus.gitdata@gmail.com>
2019-12-14 13:22:18 +0700

    Add input Listener

--- src/containers/App.js
+++ src/containers/App.js
@@ -18,17 +18,29 @@ const paramsOption = {
   }
 };
 
-function App() {
-  return (
-    <div className="App">
-      <Particles className="particles" params={paramsOption} />
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm />
-      {/* {<FaceRecognition />} */}
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      input: ""
+    };
+  }
+
+  onInputChange = event => {
+    console.log(event.target.value);
+  };
+  render() {
+    return (
+      <div className="App">
+        <Particles className="particles" params={paramsOption} />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm onInputChange={this.onInputChange} />
+        {/* {<FaceRecognition />} */}
+      </div>
+    );
+  }
 }
 
 export default App;
