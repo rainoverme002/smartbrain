39d23c8bc3df37e48e36ab314d14a16a991d88dc
rainoverme002 <bagus.gitdata@gmail.com>
2019-12-14 12:40:31 +0700

    Add Particles.js

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,15 +3,28 @@ import Navigation from "../components/Navigation";
 import Logo from "../components/Logo";
 import ImageLinkForm from "../components/ImageLinkForm";
 import Rank from "../components/Rank";
-
+import Particles from "react-particles-js";
 import "./App.css";
+
+const paramsOption = {
+  particles: {
+    number: {
+      value: 30,
+      density: {
+        enable: true,
+        value: 40
+      }
+    }
+  }
+};
 
 function App() {
   return (
     <div className="App">
+      <Particles className="particles" params={paramsOption} />
       <Navigation />
       <Logo />
-      <Rank/>
+      <Rank />
       <ImageLinkForm />
       {/* {<FaceRecognition />} */}
     </div>
