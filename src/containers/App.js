3c2e85c6d6a7c36b0a3dd77e88c639a73808b2a0
rainoverme002 <bagus.gitdata@gmail.com>
2020-01-01 23:29:51 +0700

    Add Fetch post for clarifai api to server

--- src/containers/App.js
+++ src/containers/App.js
@@ -7,12 +7,7 @@ import Rank from "../components/Rank/Rank";
 import Signin from "../components/Signin/Signin";
 import Register from "../components/Register/Register";
 import Particles from "react-particles-js";
-import Clarifai from "clarifai";
 import "./App.css";
-
-const app = new Clarifai.App({
-  apiKey: "0d8d8178cf1f473991c03d35c2c49394"
-});
 
 const paramsOption = {
   particles: {
@@ -92,8 +87,14 @@ class App extends React.Component {
 
   onButtonSubmit = () => {
     this.setState({ imageUrl: this.state.input });
-    app.models
-      .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+    fetch("http://localhost:3000/imageurl", {
+      method: "post",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        input: this.state.input
+      })
+    })
+      .then(response => response.json())
       .then(response => {
         if (response) {
           fetch("http://localhost:3000/image", {
@@ -108,9 +109,7 @@ class App extends React.Component {
               this.setState(Object.assign(this.state.user, { entries: count }));
             });
         }
-        this.displayBoundingBox(
-          this.calculateBoundingBox(response)
-        ).catch(err => console.log(err));
+        this.displayBoundingBox(this.calculateBoundingBox(response));
       });
   };
 
